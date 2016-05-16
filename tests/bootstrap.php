c004fe7bfd6a3c93ae2a7f6fd8e97e9a534819c4
Johannes Wachter <johannes.wachter@massiveart.at>
2016-05-16 13:54:12 +0200

    added travis config

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$file = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+$loader = require $file;
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+return $loader;
