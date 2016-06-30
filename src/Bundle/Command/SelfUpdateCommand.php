1638d2d5c8a34a3131fd420d2b4c12a91e090d21
Johannes Wachter <johannes.wachter@massiveart.at>
2016-06-30 18:58:21 +0200

    added update channels (stable, nightly)

--- src/Bundle/Command/SelfUpdateCommand.php
+++ src/Bundle/Command/SelfUpdateCommand.php
@@ -2,9 +2,11 @@
 
 namespace Nanbando\Bundle\Command;
 
+use Humbug\SelfUpdate\Strategy\GithubStrategy;
 use Humbug\SelfUpdate\Updater;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SelfUpdateCommand extends Command
@@ -16,7 +18,8 @@ class SelfUpdateCommand extends Command
     {
         $this
             ->setName('self-update')
-            ->setDescription('Updates the application.');
+            ->setDescription('Updates the application.')
+            ->addOption('nightly', null, InputOption::VALUE_NONE, 'Force an update to nightly channel');
     }
 
     /**
@@ -25,21 +28,47 @@ class SelfUpdateCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $updater = new Updater();
-        $updater->getStrategy()->setPharUrl('http://nanbando.github.io/core/nanbando.phar');
-        $updater->getStrategy()->setVersionUrl('http://nanbando.github.io/core/nanbando.phar.version');
+
+        if ($input->getOption('nightly')) {
+            $this->nightly($updater);
+        } else {
+            $this->stable($updater);
+        }
 
         $result = $updater->update();
         if (!$result) {
-            $output->writeln('You are already using composer version ' . $updater->getNewVersion());
+            $output->writeln('You are already using "' . $updater->getNewVersion() . '" version.');
 
             // No update needed!
             return;
         }
 
-        $new = $updater->getNewVersion();
-        $old = $updater->getOldVersion();
+        $output->writeln(sprintf('Updated from %s to %s', $updater->getOldVersion(), $updater->getNewVersion()));
+    }
 
-        $output->writeln(sprintf('Updated from %s to %s', $old, $new));
+    /**
+     * Configure updater to use nightly builds.
+     *
+     * @param Updater $updater
+     */
+    private function nightly(Updater $updater)
+    {
+        $updater->getStrategy()->setPharUrl('http://nanbando.github.io/core/nanbando.phar');
+        $updater->getStrategy()->setVersionUrl('http://nanbando.github.io/core/nanbando.phar.version');
+    }
+
+    /**
+     * Configure updater to use unstable builds.
+     *
+     * @param Updater $updater
+     */
+    private function stable(Updater $updater)
+    {
+        $updater->setStrategy(Updater::STRATEGY_GITHUB);
+        $updater->getStrategy()->setPackageName('nanbando/core');
+        $updater->getStrategy()->setPharName('nanbando.phar');
+        $updater->getStrategy()->setCurrentLocalVersion('@git_version@');
+        $updater->getStrategy()->setStability(GithubStrategy::STABLE);
     }
 
     /**
